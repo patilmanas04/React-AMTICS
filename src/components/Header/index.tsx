f72609d37e1a379a770dd7cce840811905267f66
bendalejatin <bendalejatin35@gmail.com>
2024-04-07 15:45:15 +0530

    add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,23 @@
+import React from 'react';
+import './style.css';
+import clglogo from '../../images/AMTICSlogo.png';
+import unilogo from '../../images/UTUlogo.png';
+
+
 const Header = () => {
   return (
-    <div>Header Section</div>
-  )
-}
+    <header>
+        <div id='clglogo'>
+            <img src={clglogo} alt="College Logo"/>
+        </div>
+        <div id='clgname'>
+            <h1>Asha M. Tarsadia Institute of Computer Science and Technology</h1>
+        </div>
+        <div id='unilogo'>
+            <img src={unilogo} alt="University Logo" />
+        </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
